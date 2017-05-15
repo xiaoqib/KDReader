17e6d8218b2fec4e678d46552bc0249154311c22
陳升琪 <434494016@qq.com>
2017-05-15 18:55:06 +0800

    Initial Commit

---
+++ KDReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KDReader
+//
+//  Created by 陳升琪 on 2017/5/15.
+//  Copyright © 2017年 陳升琪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KDReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KDReader
+//
+//  Created by 陳升琪 on 2017/5/15.
+//  Copyright © 2017年 陳升琪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
